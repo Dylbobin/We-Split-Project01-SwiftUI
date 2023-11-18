d565547123e58e45dce78c9d07c244205a92af06
Dylbobin <dylansilva@csu.fullerton.edu>
2023-11-18 11:55:49 -0800

    Initial Commit

---
+++ We Split/We_SplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  We_SplitApp.swift
+//  We Split
+//
+//  Created by Dylan Silva on 11/18/23.
+//
+
+import SwiftUI
+
+@main
+struct We_SplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
